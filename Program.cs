551b7f0528acfa0f85790ef02eb77c8496060f01
Jorge Luis <jorge.luz@ufpi.edu.br>
2025-04-03 17:02:09 -0300

    Algumas atts
    Atualizando o Cores da aplicação

--- Program.cs
+++ Program.cs
@@ -1,25 +1,34 @@
 using Anuncia_Picos.Components;
 using System.Net.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configurações do CORS para permitir qualquer origem
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() // Permitir qualquer origem
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+// Adiciona os componentes do Blazor
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura o pipeline de requisição HTTP
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseStaticFiles();  // Para servir arquivos estáticos como JS, CSS, imagens
+app.UseCors("AllowAll");  // Aplica o CORS para permitir acessos de qualquer origem
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
